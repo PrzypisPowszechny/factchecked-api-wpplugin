3ed04cc9a75d274b13a927d380bdc6986b60bab8
Tomek Glowka <tk.glowka@gmail.com>
2018-11-23 19:33:45 +0100

    Add location checking for one text on one page

--- scraper.php
+++ scraper.php
@@ -4,8 +4,34 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Goutte\Client;
 
+function universalizeSpaces($text) {
+    return preg_replace( '/\s+/', ' ', $text);
+}
+
+$quote = universalizeSpaces("Mamy około 250 wójtów, burmistrzów, prezydentów z listy Polskiego Stronnictwa");
+
+$url = 'https://www.rmf24.pl/tylko-w-rmf24/poranna-rozmowa/news-kosiniak-kamysz-samorzady-powinny-miec-wiekszy-udzial-w-vat,nId,2632608';
+
 $client = new Client();
-$crawler = $client->request('GET', 'https://www.symfony.com/blog/');
-$crawler->filter('h2 > a')->each(function ($node) {
-    print $node->text()."\n";
-});
+$crawler = $client->request('GET', $url);
+$document = $crawler->getNode(0)->ownerDocument;
+
+$xpath = new DOMXpath($document);
+foreach ($xpath->evaluate('//script') as $node) {
+    $node->parentNode->removeChild($node);
+}
+
+$scriptlessHtml = $document->saveHtml($document->documentElement);
+$pureText = strip_tags($scriptlessHtml);
+
+$universalText = universalizeSpaces($pureText);
+
+
+$located = strpos($universalText, $quote);
+
+if ($located === FALSE) {
+    print 'NO';
+} else {
+    print 'YES';
+}
+print "\n";
