2e3fac3035511443d8af1701c054afb7fe944f53
Tomek Glowka <tk.glowka@gmail.com>
2018-11-26 19:34:16 +0100

    Stabilize scraping by truncating and buffering

--- scraper.php
+++ scraper.php
@@ -6,7 +6,7 @@ use Goutte\Client;
 use GuzzleHttp\Exception\ConnectException;
 
 
-if ( ! function_exists('write_log')) {
+if (!function_exists('write_log')) {
     function write_log ( $log )  {
         if ( is_array( $log ) || is_object( $log ) ) {
             error_log( print_r( $log, true ) );
@@ -42,16 +42,18 @@ function get_located_statement($statement, $url) {
 
 
 function is_statement_in_content($statement, $content) {
-    return strpos(universalizeSpaces($content), universalizeSpaces($statement)) !== FALSE;
+    return strpos(universalizeSpaces($content), universalizeSpaces($statement, TRUE)) !== FALSE;
 }
 
 
 function get_page_content($url) {
-    //write_log('Getting page content');
+    // write_log('Getting page content');
     $client = new Client();
     // TODO: we should use lower level tool for fetching websites as we lack some low level info
     try {
-        $crawler = $client->request('GET', $url);
+        $crawler = $client->request('GET', $url, [
+            'connect_timeout' => 10.0, 'read_timeout' => 10.0, 'timeout' => 10.0
+        ]);
     } catch (ConnectException $e) {
         throw new URLFetchingException('Unable to connect to the URL, it is either incorrect or was denied');
     } catch (Exception $e) {
@@ -73,12 +75,27 @@ function get_page_content($url) {
 }
 
 
-function universalizeSpaces($text) {
+function universalizeSpaces($text, $remove_brackets = FALSE) {
+    // Some texts (http responses) happen to be very big, so truncate them and preg_replace using buffering.
+    $BUF_SIZE = 100000; // 100KB
+    $MAX_TEXT_SIZE = 2 * 1000 * 1000; // 2MB
+    $text = substr($text, 0, $MAX_TEXT_SIZE);
+    $new_text = '';
+    for($i = 0; $i < strlen($text); $i += $BUF_SIZE) {
+        $part = substr($text, $i, $BUF_SIZE);
+        $part = preg_replace( '/\s+/', ' ', $part);
+        $new_text .= $part;
+    }
+    $text = $new_text;
+    // Now, no big replaces, so we can run it safely
     $text = preg_replace( '/\s+/', ' ', $text);
+
     $text = strtolower($text);
 
-    // delete [ ] brackets containing context provided by editorial team
-    $text = trim(preg_replace('/\s*\[[^\]]+\](\s*)/', '$1', $text));
+    if ($remove_brackets) {
+        // delete [ ] brackets containing context provided by editorial team
+        $text = trim(preg_replace('/\s*\[[^\]]+\](\s*)/', '$1', $text));
+    }
     return $text;
 }
 
